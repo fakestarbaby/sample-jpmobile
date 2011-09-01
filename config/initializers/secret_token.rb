b4b9bfb81100485cf7990d4a6c7492f2430ceee5
fakestarbaby <fakestarbaby@gmail.com>
2011-09-01 11:57:54 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleJpmobile::Application.config.secret_token = '1269e5680a0da6c91228f8d6f73abd4833ab9c77d5ebf562269661a899061a3ab9b3a1031391e8ab7b6c906111e12f3af3201b928f60c0f42bdf20f62648538b'
